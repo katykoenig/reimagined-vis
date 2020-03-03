2021767d0cf390ee2ab3c08b414e6ebc73f25395
Katy Koenig <kkoenig810@gmail.com>
2020-03-02 21:08:47 -0600

    updated docstrings for font_rules

--- v_a11y_lint/font_rules.py
+++ v_a11y_lint/font_rules.py
@@ -7,6 +7,12 @@ import util_fns
 
 def check_title(chart_obj):
     '''
+    Checks to see if chart has title
+
+    Inputs:
+        chart_obj: Altair chart object
+
+    Outputs: string with title issue (if relevant)
     '''
     if type(chart_obj.title) == alt.utils.schemapi.UndefinedType:
         return 'chart needs title'
@@ -15,7 +21,14 @@ def check_title(chart_obj):
 def check_font_size(font_size_dict, thres=16):
     '''
     Checks that all fonts are above a threshold value
-    (Set at 16 b/c that's 1.2 em <- used by WCAG)
+    Note:
+        threshold value of 16 = 1.2 em, this is the guideline set by WCAG 2.0
+
+    Inputs:
+        font_size_dict: dictionary with all font aspects to be checked
+        thres: int representing min. font size
+
+    Outputs: string with size issue (if relevant)
     '''
     for key, val in font_size_dict.items():
         if 'Size' in key:
@@ -26,6 +39,12 @@ def check_font_size(font_size_dict, thres=16):
 def check_title_length(title, min_len=10):
     '''
     Checks if chart has descriptive title
+
+    Inputs:
+        title(str): chart's type
+        min_len(int): min. desired length of titles
+
+    Outputs: string with title issue (if relevant)
     '''
     if len(title) < min_len:
         return 'chart title lacks description'
@@ -33,6 +52,13 @@ def check_title_length(title, min_len=10):
 
 def check_all_font(font_dict):
     '''
+    Checks all fonts in a chart to ensure that font size and title
+    combinations are accessible
+
+    Inputs:
+        font_dict: a dictionary of the chart's font features to be checked
+
+    Output: a dictionary representing the font issues of the chart
     '''
     issues = {}
     for key, val in font_dict.items():
